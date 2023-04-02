903c71bf616f8a2965612fbe42078860f0bdaa52
Luis Garcia <luisarmandogarcia96@gmail.com>
2023-04-02 11:56:25 +0200

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,36 @@
-import React, { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
 import { Sub } from './types';
+import INITIAL_STATE from './data/subs.json';
 
 interface AppState {
   subs: Array<Sub>,
   newSubsNumber: number
 }
 
-const INITIAL_STATE = [
-  {
-    nick: 'dapelu',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/150?u=dapelu',
-    description: 'Dapelu hace de moderador a veces'
-  },
-  {
-    nick: 'sergio_serrano',
-    subMonths: 7,
-    avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',
-  }
-];
-
 function App() {
   const [subs, setSubs] = useState<AppState["subs"]>([]);
   const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0);
   const divref = useRef<HTMLDivElement>(null);
 
+
   useEffect(() => {
     setSubs(INITIAL_STATE);
-  }, []);
+  }
+, []);
 
   const handleNewSub = (newSub: Sub): void => {
     setSubs(prev => [...prev, newSub]);
+    setNewSubsNumber(n => n + 1);
   }
 
   return (
     <div className="App" ref={divref}>
-      <h1>midu subs</h1>
+      <h1>All subscribers</h1>
         <List subs={subs} />
+        New Subs: {newSubsNumber}
         <Form onNewSub={handleNewSub}/>
     </div>
   );
